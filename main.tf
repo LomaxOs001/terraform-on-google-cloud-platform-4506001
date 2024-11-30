e61088d17a69609835b9c79b596e0f7570a28f6d
Lomax Osomba <72916140+LomaxOs001@users.noreply.github.com>
2024-11-30 03:11:27 +0000

    delete old network

--- main.tf
+++ main.tf
@@ -25,18 +25,6 @@ module "app_network" {
 
 }
 
-resource "google_compute_network" "app" {
-  name                    = var.network_name
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "app" {
-  name          = var.network_name
-  ip_cidr_range = var.network_ip_cidr_range
-  region        = var.region
-  network       = google_compute_network.app.id
-  
-}
 
 data "google_compute_image" "ubuntu" {
   most_recent = true
